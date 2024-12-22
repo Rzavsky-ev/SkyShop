1327ef1321370a79f197fdbce04a1423e8d164b7
Rzavsky-ev <rzavsky.ev@gmail.com>
2024-12-22 11:06:56 +0300

    Сделано ДЗ

---
+++ src/main/java/org/skypro/skyshop/model/controller/ShopControllerAdvice.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.model.controller;
+
+
+import org.skypro.skyshop.model.error.ShopError;
+import org.skypro.skyshop.model.exceptions.NoSuchProductException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ShopControllerAdvice {
+    @ExceptionHandler(NoSuchProductException.class)
+    public ResponseEntity<ShopError> divisionByZeroHandler(NoSuchProductException e) {
+        return new ResponseEntity<>(new ShopError("001", "Нет такого продукта."),
+                HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ src/main/java/org/skypro/skyshop/model/exceptions/NoSuchProductException.java
@@ -0,0 +1,4 @@
+package org.skypro.skyshop.model.exceptions;
+
+public class NoSuchProductException extends RuntimeException{
+}
